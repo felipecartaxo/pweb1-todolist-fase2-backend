d2cf94552f5ac0b5f65eb15f5a42d6398a516933
felipecartaxo <felipecartaxo1@gmail.com>
2025-03-17 16:32:56 -0300

    concluindo camada dos Controladores

--- src/main/java/br/edu/ifpb/cartaxo/todo_list/controller/TarefaController.java
+++ src/main/java/br/edu/ifpb/cartaxo/todo_list/controller/TarefaController.java
@@ -13,25 +13,25 @@ public class TarefaController {
 
     @Autowired private TarefaService tarefaService;
 
-    // Retorna todos os usuários
+    // Retorna todos as tarefas
     @GetMapping("/tarefas")
     public List<Tarefa> getTarefas() {
         return this.tarefaService.getTarefas();
     }
 
-    // Retorna um usuário a partir do id passado na URL
+    // Retorna uma tarefa a partir do id passado na URL
     @GetMapping("/tarefas/{id}")
     public Tarefa getTarefaPorId(@PathVariable Long id) {
         return this.tarefaService.getTarefaPorId(id);
     }
 
-    // Insere um novo usuário
+    // Insere uma nova tarefa
     @PostMapping("/tarefas")
     public Tarefa inserirTarefa(@RequestBody Tarefa tarefa) {
         return this.tarefaService.inserirOuAtualizar(tarefa);
     }
 
-    // Responde a requisições do tipo DELETE feitas para /api/users/{id}. Neste caso, irá remover o usuário cujo id for igual ao id passado na URL
+    // Responde a requisições do tipo DELETE feitas para tarefas/{id}. Neste caso, irá remover a tarefa cujo id for igual ao id passado na URL
     @DeleteMapping("tarefas/{id}")
     public void apagarTarefa(@PathVariable Long id) {
         this.tarefaService.apagar(id);
---
+++ src/main/java/br/edu/ifpb/cartaxo/todo_list/controller/UsuarioController.java
@@ -0,0 +1,39 @@
+package br.edu.ifpb.cartaxo.todo_list.controller;
+
+import br.edu.ifpb.cartaxo.todo_list.model.Usuario;
+import br.edu.ifpb.cartaxo.todo_list.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class UsuarioController {
+
+    @Autowired private UsuarioService usuarioService;
+
+    // Retorno todos os usuários
+    @GetMapping("/usuarios")
+    public List<Usuario> getUsuarios() {
+        return this.usuarioService.getUsuarios();
+    }
+
+    // Retorna um usuário a partir de um id passado na URL
+    @GetMapping("/usuarios{id}")
+    public Usuario getUsuarioPorId(@PathVariable Long id) {
+        return this.usuarioService.getUsuarioPorId(id);
+    }
+
+    // Insere um atualiza um novo usuário
+    @PostMapping("/usuarios")
+    public Usuario inserirUsuario(@RequestBody Usuario usuario) {
+        return this.usuarioService.inserirOuAtualizar(usuario);
+    }
+
+    // Remove um usuário a partir de um id
+    @DeleteMapping("tarefas/{id}")
+    public void apagarUsuario(@PathVariable Long id) {
+        this.usuarioService.apagar(id);
+    }
+}
